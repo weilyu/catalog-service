5efff459980225978b774b5fd3595e4fc8bdf7e3
Wei LYU <lvwei1001@gmail.com>
2023-02-21 05:23:36 +0000

    Read greeting message from app config

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -3,12 +3,20 @@ package com.polarbookshop.catalogservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+
 @RestController
 public class HomeController {
 
+	PolarProperties polarProperties;
+
+	public HomeController(PolarProperties polarProperties) {
+		this.polarProperties = polarProperties;
+	}
+
 	@GetMapping("/")
 	public String getGreeting() {
-		return "Welcome to the book catalog!";
+		return polarProperties.getGreeting();
 	}
 
 }
