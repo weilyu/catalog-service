01b969b07a9b76c8e08ca090bf61ec98cab928e1
Wei Lyu <lvwei1001@gmail.com>
2023-02-22 22:46:08 +0900

    Add tests for jdbc book repository

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,31 +1,36 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
     @Autowired
     private WebTestClient webTestClient;
 
-//    @Test
-//    void whenPostRequestThenBookCreated() {
-//        var expectedBook = Book.of("1231231231", "Title", "Author", 9.90);
-//
-//        webTestClient
-//                .post()
-//                .uri("/books")
-//                .bodyValue(expectedBook)
-//                .exchange()
-//                .expectStatus().isCreated()
-//                .expectBody(Book.class).value(actualBook -> {
-//                    assertThat(actualBook).isNotNull();
-//                    assertThat(actualBook.isbn())
-//                            .isEqualTo(expectedBook.isbn());
-//                });
-//    }
+    @Test
+    void whenPostRequestThenBookCreated() {
+        var expectedBook = Book.of("1231231231", "Title", "Author", 9.90);
+
+        webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(expectedBook)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(actualBook -> {
+                    Assertions.assertThat(actualBook).isNotNull();
+                    Assertions.assertThat(actualBook.isbn())
+                            .isEqualTo(expectedBook.isbn());
+                });
+    }
 }
